e39d9c9e30bc5f4782e5e76edb7eb7e4d0bd9917
kaushalyaguesthouse <sy687321@gmail.com>
2026-07-25 14:56:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ process.env.SUPABASE_ANON_KEY
 
 const razorpay = new Razorpay({
 
-key_id:process.env.RAZORPAY_KEY_ID,
+key_id: "rzp_live_THSQSxsz10dlWB",
 
 key_secret:process.env.RAZORPAY_KEY_SECRET
 
